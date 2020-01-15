7125a01128b383374d58cc5b59af97a41be25af7
Christian Berck <christian@berck.org>
2020-01-15 16:11:45 -0600

    Added 2 pickups
    1 Pickup revolves and 1 changes scale

---
+++ Assets/Scripts/Revolve.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Revolve : MonoBehaviour
+{
+    
+    private float radius = 2.45f;
+    private float newAngle;
+    // Update is called once per frame
+
+    void Update()
+    {
+        
+        
+        newAngle += 5f* Time.deltaTime;
+        transform.position = new Vector3(radius*Mathf.Sin(newAngle), 0.5f, radius*Mathf.Cos(newAngle));
+    }
+}
