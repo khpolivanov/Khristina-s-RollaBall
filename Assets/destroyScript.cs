ed59c6ad71a69aa76b5e2494630b221d34f82485
khpolivanov <31524380+khpolivanov@users.noreply.github.com>
2020-01-24 16:40:43 -0600

    Added Longbow
    Tried to add longbow and arrows to disappear when hit pickup

---
+++ Assets/destroyScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
